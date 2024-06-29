063b8dba8dd5a7a9bc78aeab7a09c5fbd45019be
Amol Shinde <shindeamol410@gmail.com>
2024-06-29 23:37:58 +0530

    practice

---
+++ practice.py
@@ -0,0 +1,77 @@
+# def factorial(num):
+#     result = 1
+#     i = 1
+
+#     if num < 0 :
+#         print ("factorial is not defined for negative numbers")
+#         return None
+    
+#     while i <= num:
+#         result *= i
+#         i += 1
+#     return result    
+
+# num = input("Enter your number:- ")
+
+# num = int(num)
+
+# result = factorial(num)
+
+# print(result)
+
+
+
+
+# def factorial(num):
+#     result =1
+#     i = 1
+
+#     if num < 0:
+#         print("please dont enter negative number")
+#         return None
+
+#     while i <= num:
+#         result *= i 
+#         i += 1
+
+#     return result
+
+# num = int(input("enter your number here:- ") )
+# fact = factorial(num)
+
+# print(fact)
+
+
+# def factorial(num):
+#     result = 1
+#     i = 1
+
+#     if num < 0 :
+#         print("Factorial not defined for negative numbers")
+#         return None
+    
+#     while i <= num:
+#         result *= i
+#         i += 1
+#     return result    
+
+# num = int(input("enter number here :-"))
+
+# result = factorial(num)    
+
+# print(result)
+
+
+# def factorial(num):
+#     result = 1
+
+#     for i in range(1, num+1):
+#         result *= i
+
+#     return result 
+
+# num = int(input("enter number here :-"))
+
+# result = factorial(num)    
+
+# print(result)    
